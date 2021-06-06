b4d1eb7969529349e99b3d43706d4a65ddff718e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-06 22:16:16 +0900

    add onchage, reset button, and so on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,17 @@ import React, { useState} from 'react'
 const App = props  => {
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
       <p>現在の{name}は、{price}です</p>
+      <button onClick={() => setPrice(price+1)}>+1</button>
+      <button onClick={() => setPrice(price-1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   )
 }
