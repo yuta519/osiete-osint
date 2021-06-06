e3c1adc9ee28fb8fca1e0e75e787a8813555c26d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-06 21:56:52 +0900

    tutorial for using props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,19 @@
 import React, { useState} from 'react'
 
-const App = ()  => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count+1)
-  const decrement = () => setCount(count-1)
-  const increment2 = () => setCount(previousCount => previousCount+1)
-  const decrement2 = () => setCount(previousCount => previousCount-1)
-  const reset = () => setCount(currentCount => currentCount=0)
-  const waru = () => setCount(currentCount => 
-    currentCount%3===0 ? currentCount/3: currentCount)
-
+const App = props  => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-      </div>
-      <div>
-        <button onClick={waru}>3の倍数だけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}です</p>
     </>
   )
 }
+
+App.defaultProps = {
+  name: '',
+  price: 1000
+}
+
 
 export default App
