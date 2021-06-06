dc8c7b282f1c1ef84846aa58877ca0bc420655e0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-06 20:10:22 +0900

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 
 import App from './components/App';
 import HotOsints from './components/HotOsints'; 
+import Nav from './components/layout/Nav';
 import reportWebVitals from './reportWebVitals';
 import reducer from './reducers';
 
@@ -12,6 +13,10 @@ const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
+    <React.StrictMode>
+      <Nav />
+    </React.StrictMode>
+    <App />
     <HotOsints />
   </Provider>,
   document.getElementById('root')
