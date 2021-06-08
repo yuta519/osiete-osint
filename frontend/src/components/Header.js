efb107896b111b4bbb70c3b97e6af3ab6b568015
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-08 21:31:21 +0900

    create Header.js at new place

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function Header() {
+  return (
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fuid">
+        <a className="navbar-brand" href="#"> 
+          OSIETE OSINT
+        </a>
+        </div>
+      </nav>
+  )
+}
+
+export default Header;
