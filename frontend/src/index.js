9452383108a99151d25d4433e7fdd52783dfa17f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-09 21:51:42 +0900

    implement a new component to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-import App from './components/App';
+import App from './components/App'
 // import HotOsints from './components/HotOsints'
 import Header from './components/Header'
+import reducer from './reducers'
+import SearchOsint from './components/SearchOsint'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
+    {/* <App /> */}
+    <SearchOsint />
     {/* <HotOsints /> */}
   </React.StrictMode>,
   document.getElementById('root')
