801f6d0947af5312e36c7d10589800aad478bc3c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-09 21:53:04 +0900

    implementing new feature to action and reducer

--- frontend/src/reducers/osints.js
+++ frontend/src/reducers/osints.js
@@ -1,1 +1,13 @@
-import { FETCH_OSINTS, SEARCH_OSINT } from "../actions/OsintAction"
+import { FETCH_OSINTS, SEARCH_OSINT } from "../actions/osintAction"
+
+
+export default(state = {}, action) => {
+    switch (action.type) {
+        case FETCH_OSINTS:
+            return state
+        case SEARCH_OSINT:
+            return state
+        default:
+            return state
+    }
+}
