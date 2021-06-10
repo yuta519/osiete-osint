efda6afad8c790778d2efd06b9e7ce752056d97f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-10 21:35:14 +0900

    convert type and risk to readable state

--- frontend/src/components/HotOsints.js
+++ frontend/src/components/HotOsints.js
@@ -14,30 +14,35 @@ function HotOsints() {
 
   return (
     <div className='container'>
-
       <h4>Hot OSINT</h4>
       <table className="table table-hover table-striped">
         <thead>
-          <th>No.</th>
-          <th>OSINT</th>
-          <th>Type</th>
-          <th>Risk</th>
-          <th>Last Updated</th>
+          <tr>
+            {/* <th>No.</th> */}
+            <th>OSINT</th>
+            <th>Type</th>
+            <th>Risk</th>
+            <th>Last Updated</th>
+          </tr>
         </thead>
         <tbody>
           { osints.map((osint, index) => (
-            <tr id={index}>
-              <td>{ index+1 }</td>
-              <td>{ osint.data_id }</td>
-              <td>{ osint.analyzing_type }</td>
-              <td>{ osint.malicious_level }</td>
-              <td>{ osint.last_analyzed }</td>
+            <tr key={index}>
+              {/* <td>{ index+1 }</td> */}
+              <td><a href={"osint/"+osint.data_id}>{osint.data_id}</a></td>
+              { osint.analyzing_type === 1? (<td>IP Address</td>) 
+              : osint.analyzing_type === 2? (<td>URL</td>) 
+              : osint.analyzing_type === 3? (<td>File Hash</td>) 
+              : (<td></td>) }
+              { osint.malicious_level === 1? (<td>MALICIOUS</td>)
+              : osint.malicious_level === 2? (<td>SUSPICIOUS</td>) 
+              : osint.malicious_level === 3? (<td>SAFE</td>) 
+              : (<td></td>) }
+              <td>{osint.last_analyzed}</td>
             </tr>
-          )) }
+          ))}
         </tbody>
-
       </table>
-        
     </div>
   );
 }
