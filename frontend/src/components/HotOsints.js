783ad2e0566da3096f578d9041eadc076ebcb315
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-10 10:34:48 +0900

    display all osints on table at HotOsints.js

--- frontend/src/components/HotOsints.js
+++ frontend/src/components/HotOsints.js
@@ -5,16 +5,18 @@ import fetch_osints from '../actions/osintAction'
 import axios from 'axios'
 
 function HotOsints() {
+  const [osints, setOsints] = useState([])
   useEffect(async () => {
     const response = await axios.get('http://localhost:8000/api/data')
-    console.log(response.data)
-  })
-  const [osints] = useState('')
+    setOsints(response.data)
+  }, [])
+  console.log(osints)
+
   return (
-    <div className='container-fluid'>
+    <div className='container'>
 
       <h4>Hot OSINT</h4>
-      <table className="table table-hover">
+      <table className="table table-hover table-striped">
         <thead>
           <th>No.</th>
           <th>OSINT</th>
@@ -23,11 +25,19 @@ function HotOsints() {
           <th>Last Updated</th>
         </thead>
         <tbody>
-
+          { osints.map((osint, index) => (
+            <tr id={index}>
+              <td>{ index+1 }</td>
+              <td>{ osint.data_id }</td>
+              <td>{ osint.analyzing_type }</td>
+              <td>{ osint.malicious_level }</td>
+              <td>{ osint.last_analyzed }</td>
+            </tr>
+          )) }
         </tbody>
 
       </table>
-      
+        
     </div>
   );
 }
