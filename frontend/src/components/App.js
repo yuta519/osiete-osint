885977d933b7b521e456fdf634870a6aa2e95c99
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-11 15:58:07 +0900

    render componets are organized into app.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,67 +1,8 @@
-import React, { useReducer, useState } from 'react'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import reducer from '../reducers'
-
+import HotOsints from './HotOsints'
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, [])
-  const [osint, setOsint] = useState('')
-  const [type, setType] = useState('')
-  
-  const searchOsint = e => {
-    // e.prevetDefault()
-    console.log("search")
-    console.log({osint, type})
-    dispatch({
-      type: 'SEARCH_OSINT',
-      osint,
-      type
-    })
-  }
-
   return (
-    <div className='container'>
-
-      <h4>Search OSINT</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventOsint">OSINT</label>
-          <input className="form-control" id="formEventOsint" value={ osint } onChange={e => setOsint(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="formEventType">Type</label>
-          <select className="form-control" id="formEventType" onChange={e => setType(e.currentTarget.value)}>
-            <option value="">---</option>
-            <option value="1">IP Address</option>
-            <option value="2">URL</option>
-            <option value="3">File Hash</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="formEventBody">Comment</label>
-          <textarea className="form-control" id="formEventBody" />
-        </div>
-        <button className="btn btn-primary" onClick={ searchOsint }>search</button>
-      </form>
-
-      {/* <h4>OSINT List</h4>
-      <table className="table table-hover">
-        <thead>
-          <th>No</th>
-          <th>OSINT</th>
-          <th>Type</th>
-          <th>Risk</th>
-          <th>Last Updated</th>
-        </thead>
-        <tbody>
-
-        </tbody>
-
-      </table> */}
-      
-    </div>
+    <HotOsints />
   )
 }
 
