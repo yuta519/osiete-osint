5de79403a2bec7cd3c8d83e86a17316626ccc874
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-11 23:25:45 +0900

    add new componet About.js

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+  <>
+    <div className="container">
+      <h4>What is OSIETE OSINT</h4>
+    </div>
+  </>
+  ) 
+}
+
+export default About
