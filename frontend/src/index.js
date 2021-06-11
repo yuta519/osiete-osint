11119f775db8a52e6d31377cd505c729d7ed06d2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-11 23:06:00 +0900

    add react-router library

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import App from './components/App'
 import Header from './components/Header'
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <Router>
+        <Route path="/" exact><App /></Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
