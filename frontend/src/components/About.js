00cdf53f72dc836d8870c05930c6a2e0d359c0fa
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-11 21:58:37 +0900

    add about.js

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+      <div className='container-fluid'>
+        <h4>What is OSIETE OSINT</h4>
+      </div>
+    </>
+  )
+}
+
+export default About
