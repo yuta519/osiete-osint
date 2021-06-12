b6f0986f05c5f66e3d2ac2a5acdae8b8a159133c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-12 12:33:59 +0900

    improve invalid href value

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-light" style={{ marginTop:'10%' }}>
       <div className="container">
-        <span className="text-muted">Copyright <a href="">OSIETE OSINT</a>, by <a href="https://github.com/yuta519/osiete-osint.git">@yuta519</a>.</span>
+        <span className="text-muted">Copyright <a href="/">OSIETE OSINT</a>, by <a href="https://github.com/yuta519/osiete-osint.git">@yuta519</a>.</span>
       </div>
     </footer>
     
