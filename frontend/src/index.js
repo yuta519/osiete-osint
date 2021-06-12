6b48faccae079e95e61a3cbd24200c742d86114e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-12 11:44:20 +0900

    add OsintDetail.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import About from './components/About'
 import App from './components/App'
+import OsintDetail from './components/OsintDetail'
 import SearchOsint from './components/SearchOsint'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -13,8 +14,9 @@ ReactDOM.render(
     <Header />
     <Router>
         <Route path="/" exact><App /></Route>
-        <Route path="/what_is_osiete_osint" exact><About /></Route>
+        <Route path="/osint"><OsintDetail /></Route>
         <Route path="/search_osint" exact><SearchOsint /></Route>
+        <Route path="/what_is_osiete_osint" exact><About /></Route>
     </Router>
     <Footer />
   </React.StrictMode>,
