d91a4079970aa8cd8d3880f390af68f445cff69d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-13 21:28:13 +0900

    change indent

--- frontend/src/reducers/osints.js
+++ frontend/src/reducers/osints.js
@@ -1,13 +1,13 @@
 import { FETCH_OSINTS, SEARCH_OSINT } from "../actions/osintAction"
 
 
-export default(state = {'test': 'yeaaar'}, action) => {
-    switch (action.type) {
-        case FETCH_OSINTS:
-            return state
-        case SEARCH_OSINT:
-            return state
-        default:
-            return state
-    }
+export default(state = {'osints': 'yeaaar'}, action) => {
+  switch (action.type) {
+    case FETCH_OSINTS:
+      return state
+    case SEARCH_OSINT:
+      return state
+    default:
+      return state
+}
 }
