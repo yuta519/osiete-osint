048a0f4313fd8e12562d298b7636eb3ef280247b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-14 21:37:22 +0900

    change style for footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div style={{ "margin-top": "50px" }}>
-      <footer className="footer mt-auto py-3 bg-light" style={{ "margin-top": "11150px" }}>
+      <footer className="footer mx-auto py-3 bg-light" style={{ "position": "absolute", "bottom": "0", "width": "100%" }}>
         <div className="container">
           <span className="text-muted">
             Copyright <a href="/">OSIETE OSINT</a>, by <a href="https://github.com/yuta519/osiete-osint.git">@yuta519</a>.
           </span>
         </div>
       </footer>
-    </div>
-)
+  )
 }
 
 export default Footer
