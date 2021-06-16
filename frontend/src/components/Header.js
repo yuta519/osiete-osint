748fe5acd7106be09ad0ac216fb7a44be9270cea
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-16 22:05:00 +0900

    improve style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,21 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function Header() {
   return (
     <>
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark bg-dark fixed-top flex-md-nowrap p-0 shadow">
         <div className="container-fuid">
         <a className="navbar-brand" href="/"> 
           OSIETE OSINT
         </a>
         </div>
       </nav>
-      {/* <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item"><a href="#">Home</a></li>
-          <li className="breadcrumb-item"><a href="#">Library</a></li>
-          <li className="breadcrumb-item active" aria-current="page">Data</li>
-        </ol>
-      </nav> */}
-      <ul className="nav justify-content-end">
+      <ul className="nav justify-content-end" style={{'margin-top': '30px'}}>
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Hot OSINTs</a>
         </li>
