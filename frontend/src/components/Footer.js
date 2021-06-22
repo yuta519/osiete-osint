c1c30a897dda580fd8dcb282dd02227cb5f0d81f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-22 18:40:01 +0900

    add new line for too long line

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    // style={{ "position": "absolute", "bottom": "0", "width": "100%" }}
       <footer className="footer mx-auto py-3 bg-light fixed-bottom">
         <div className="container">
           <span className="text-muted">
-            Copyright <a href="/">OSIETE OSINT</a>, by <a href="https://github.com/yuta519/osiete-osint.git">@yuta519</a>.
+            Copyright <a href="/">OSIETE OSINT</a>, 
+            by <a href="https://github.com/yuta519/osiete-osint.git">@yuta519</a>.
           </span>
         </div>
       </footer>
