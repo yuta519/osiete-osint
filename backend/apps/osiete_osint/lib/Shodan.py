5c3d723f8d6cacafd33fe770cf87bdd8ffca72c9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-07-14 23:03:46 +0900

    create ShodanClient class

--- backend/apps/osiete_osint/lib/Shodan.py
+++ backend/apps/osiete_osint/lib/Shodan.py
@@ -1,18 +1,24 @@
 from shodan import Shodan
 import os
 
-apikey = os.environ.get('SH_API')
-api = Shodan(apikey)
 
-# Lookup an IP
-ipinfo = api.host('8.8.8.8')
-print(ipinfo)
+class ShodanClient(object):
 
-# Search for websites that have been "hacked"
-for banner in api.search_cursor('http.title:"hacked by"'):
-    print(banner)
+    def __init__(self) -> None:
+        # super().__init__()
+        self.api = Shodan(os.environ.get('SH_API'))
 
-# Get the total number of industrial control systems services on the Internet
-ics_services = api.count('tag:ics')
-print('Industrial Control Systems: {}'.format(ics_services['total']))
+
+    def test(self):
+        # Lookup an IP
+        ipinfo = self.api.host('8.8.8.8')
+        print(ipinfo)
+
+        # Search for websites that have been "hacked"
+        for banner in self.api.search_cursor('http.title:"hacked by"'):
+            print(banner)
+
+        # Get the total number of industrial control systems services on the Internet
+        ics_services = self.api.count('tag:ics')
+        print('Industrial Control Systems: {}'.format(ics_services['total']))
 
